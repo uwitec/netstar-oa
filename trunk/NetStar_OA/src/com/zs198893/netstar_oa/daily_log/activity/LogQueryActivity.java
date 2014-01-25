33f1fa9b2f9bab53a34d281268e62a31ae5744bb
zs198893@gmail.com <zs198893@gmail.com@e0ee674b-a218-6828-8a82-05cf1128f3ba>
2014-01-25 09:52:02 +0000

    oa日志查询模块完成，
    oa日志录入模块，开发完毕，但是测试不通过，返回接口有问题

--- trunk/NetStar_OA/src/com/zs198893/netstar_oa/daily_log/activity/LogQueryActivity.java
+++ trunk/NetStar_OA/src/com/zs198893/netstar_oa/daily_log/activity/LogQueryActivity.java
@@ -1,8 +1,13 @@
 package com.zs198893.netstar_oa.daily_log.activity;
 
 import roboguice.inject.InjectView;
+import android.R.anim;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,13 +17,19 @@ import android.widget.Toast;
 
 import com.zs198893.netstar_oa.BaseActivity;
 import com.zs198893.netstar_oa.R;
+import com.zs198893.netstar_oa.config.DefaultConfig;
 import com.zs198893.netstar_oa.daily_log.engine.LogQueryEngine;
+import com.zs198893.netstar_oa.daily_log.engine.LogQueryEngine.QueryParamModel;
 import com.zs198893.netstar_oa.daily_log.model.DailyLogQueryListModel;
 import com.zs198893.netstar_oa.daily_log.model.DailyLogTitleModel;
 import com.zs198893.netstar_oa.model.CommonResult;
 
 public class LogQueryActivity extends BaseActivity {
 	private LogQueryEngine logQueryEngine;
+	/**
+	 * 当前的点击位置
+	 */
+	private int currentSelectPosition = -1;
 	/**
 	 * 点击监听器
 	 */
@@ -79,8 +90,8 @@ public class LogQueryActivity extends BaseActivity {
 	/**
 	 * 分页信息
 	 */
-	@InjectView(R.id.daily_log_query_page_info)
-	TextView daily_log_query_page_info;
+	@InjectView(R.id.daily_log_query_tv_page_info)
+	TextView daily_log_query_tv_page_info;
 	/**
 	 * 日期列表
 	 */
@@ -89,8 +100,8 @@ public class LogQueryActivity extends BaseActivity {
 	/**
 	 * 日志内容
 	 */
-	@InjectView(R.id.daily_log_query_tv_conent)
-	TextView daily_log_query_tv_conent;
+	@InjectView(R.id.daily_log_query_wb_conent)
+	WebView daily_log_query_wb_conent;
 
 	@Override
 	public void subInitView() {
@@ -99,17 +110,44 @@ public class LogQueryActivity extends BaseActivity {
 
 	@Override
 	public void subInitParam() {
+		daily_log_query_wb_conent.getSettings().setDefaultTextEncodingName("UTF -8") ;
+		currentSelectPosition = -1;
 		actionBar.setTitle("日志查询");
 		logQueryEngine = new LogQueryEngine(this);
 		// 初始化查询参数
 		logQueryEngine.initQueryParam();
 		// 点击监听器
 		onViewClickListener = new OnViewClickListener();
-		//初始化参数
+		// 初始化参数
 		initQueryParam();
-		//列表适配器
+		// 列表适配器
 		logDateListAdapter = new LogDateListAdapter();
 		daily_log_query_lv_date_name.setAdapter(logDateListAdapter);
+		daily_log_query_lv_date_name
+				.setOnItemClickListener(new OnItemClickListener() {
+					CommonResult commonResult;
+
+					@Override
+					public void onItemClick(AdapterView<?> arg0, View arg1,
+							int arg2, long arg3) {
+						currentSelectPosition = arg2;
+						logDateListAdapter.notifyDataSetChanged();
+						commonResult = logQueryEngine
+								.getLogContent(((DailyLogTitleModel) (logDateListAdapter
+										.getItem(arg2))).getId() + "");
+					}
+				});
+	}
+
+	/**
+	 * 显示日志内容
+	 * 
+	 * @param commonResult
+	 */
+	public void showLogContent(CommonResult commonResult) {
+		daily_log_query_wb_conent.loadData(DefaultConfig.DailyLogContentHead
+				+ (String) commonResult.getResult()
+				+ DefaultConfig.DailyLogContentFoot, "text/html; charset=UTF-8", null);
 	}
 
 	@Override
@@ -143,58 +181,120 @@ public class LogQueryActivity extends BaseActivity {
 			switch (v.getId()) {
 			// 开始日期
 			case R.id.daily_log_query_et_type_date1:
+				logQueryEngine.createDialog(daily_log_query_et_type_date1)
+						.show();
 				break;
 			// 结束日期
 			case R.id.daily_log_query_et_type_date2:
+				logQueryEngine.createDialog(daily_log_query_et_type_date2)
+						.show();
 				break;
 			// 查询按钮
 			case R.id.daily_log_query_bt_query:
+				// 拼装数据
+				QueryParamModel.departmengt = daily_log_query_et_type_department
+						.getText().toString().trim();
+				QueryParamModel.owner = daily_log_query_et_type_owner.getText()
+						.toString().trim();
+				QueryParamModel.date1 = daily_log_query_et_type_date1.getText()
+						.toString().trim();
+				QueryParamModel.date2 = daily_log_query_et_type_date2.getText()
+						.toString().trim();
+				QueryParamModel.pageIndex = 1;
 				logQueryEngine.getDateNameList();
 				break;
 			// 下一页
 			case R.id.daily_log_query_bt_nextpage:
+				checkPageIndex(QueryParamModel.pageIndex+1);
 				break;
 			// 上一页
 			case R.id.daily_log_query_bt_lastpage:
+				checkPageIndex(QueryParamModel.pageIndex-1);
 				break;
 			// 页数
 			case R.id.daily_log_query_et_page:
 				break;
 			// 跳页
 			case R.id.daily_log_query_bt_jump:
+				int index = 1;
+				try{
+					index = Integer.parseInt(daily_log_query_et_page.getText().toString().trim());
+					checkPageIndex(index);
+				}catch(Exception e){
+					daily_log_query_et_page.setText("");
+					Toast.makeText(LogQueryActivity.this, "页数格式错误", Toast.LENGTH_SHORT).show();
+				}
 				break;
 			default:
 				break;
 			}
 		}
 	}
-	/**
-	 * 初始化查询参数
-	 */
-	private void initQueryParam(){
-		
-	}
-	/**
-	 * 当数据获的完毕
-	 */
-	public void onLogListGetFinish(CommonResult commonResult){
-		if(commonResult.isSuccess()){
-			dailyLogQueryListModel = (DailyLogQueryListModel) commonResult.getResult();
-			logDateListAdapter.notifyDataSetChanged();
+	public void checkPageIndex(int pageIndex){
+		String msg = "";
+		do{
+			if(dailyLogQueryListModel==null){
+				msg = "请先查询数据";
+				break;
+			}
+			if(dailyLogQueryListModel.getTotal()<1){
+				msg = "数据为空";
+				break;
+			}
+			if(pageIndex>dailyLogQueryListModel.getTotal()){
+				msg = "不能超过最后一页";
+				break;
+			}
+			if(pageIndex<1){
+				msg = "不能小于第一页";
+				break;
+			}
+		}while(false);
+		if(TextUtils.isEmpty(msg)){
+			QueryParamModel.pageIndex = pageIndex;
+			logQueryEngine.getDateNameList();
 		}else{
-			Toast.makeText(this,(String)commonResult.getResult(), Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
 		}
 	}
 	/**
+	 * 初始化查询参数
+	 */
+	private void initQueryParam() {
+
+	}
+
+	/**
+	 * 当数据获的完毕
+	 */
+	public void onLogListGetFinish(CommonResult commonResult) {
+		if (commonResult.isSuccess()) {
+			dailyLogQueryListModel = (DailyLogQueryListModel) commonResult
+					.getResult();
+			if(dailyLogQueryListModel==null){
+				daily_log_query_tv_page_info.setText("0/0 页");
+			}else{
+				daily_log_query_tv_page_info.setText(QueryParamModel.pageIndex+"/"+dailyLogQueryListModel.getTotal()+" 页");
+			}
+			logDateListAdapter.notifyDataSetChanged();
+		} else {
+			Toast.makeText(this, (String) commonResult.getResult(),
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	/**
 	 * 时间列表适配器
 	 */
-	private class LogDateListAdapter extends BaseAdapter{
-		private Holder holder ;
+	private class LogDateListAdapter extends BaseAdapter {
+		private Holder holder;
 		private DailyLogTitleModel dailyLogTitleModel;
+
 		@Override
 		public int getCount() {
 			int count = 0;
-			if(dailyLogQueryListModel!=null&&dailyLogQueryListModel.getDailyLogTitleModel()!=null){
+			if (dailyLogQueryListModel != null
+					&& dailyLogQueryListModel.getDailyLogTitleModel() != null) {
 				count = dailyLogQueryListModel.getDailyLogTitleModel().size();
 			}
 			return count;
@@ -212,23 +312,35 @@ public class LogQueryActivity extends BaseActivity {
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			if(convertView==null){
-				convertView = View.inflate(LogQueryActivity.this, R.layout.daily_log_query_list_item, null);
+			if (convertView == null) {
+				convertView = View.inflate(LogQueryActivity.this,
+						R.layout.daily_log_query_list_item, null);
 				holder = new Holder();
 				convertView.setTag(holder);
-				holder.date = (TextView)convertView.findViewById(R.id.daily_log_query_tv_list_item_date);
-				holder.owner = (TextView)convertView.findViewById(R.id.daily_log_query_tv_list_item_owner);
-				holder.department = (TextView)convertView.findViewById(R.id.daily_log_query_tv_list_item_department);
-			}else{
+				holder.date = (TextView) convertView
+						.findViewById(R.id.daily_log_query_tv_list_item_date);
+				holder.owner = (TextView) convertView
+						.findViewById(R.id.daily_log_query_tv_list_item_owner);
+				holder.department = (TextView) convertView
+						.findViewById(R.id.daily_log_query_tv_list_item_department);
+			} else {
 				holder = (Holder) convertView.getTag();
 			}
 			dailyLogTitleModel = (DailyLogTitleModel) getItem(position);
 			holder.date.setText(dailyLogTitleModel.getCreate_Date());
 			holder.owner.setText(dailyLogTitleModel.getUserName());
 			holder.department.setText(dailyLogTitleModel.getDeptName());
+			if (currentSelectPosition == position) {
+				convertView.setBackgroundColor(getResources().getColor(
+						R.color.list_selected));
+			} else {
+				convertView.setBackgroundColor(getResources().getColor(
+						R.color.list_not_selected));
+			}
 			return convertView;
 		}
-		private  class Holder{
+
+		private class Holder {
 			public TextView date;
 			public TextView owner;
 			public TextView department;
